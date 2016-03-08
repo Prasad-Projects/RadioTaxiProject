11336d072457dbebc4d03496952ba7b1a58ed03a
Nisarg Shah <nis999999@gmail.com>
2016-03-08 12:40:11 +0530

    login and registration

--- src/Register_db.java
+++ src/Register_db.java
@@ -1,28 +1,34 @@
+import java.util.List;
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.exceptions.AlreadyExistsException;
 
 public class Register_db {
 
-	Cluster cluster;
-	Session session;
+	static Cluster cluster;
+	static Session session;
+	
+	static {
+		cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+		session = cluster.connect();
+		//	System.out.println("connection successful");
+		use_keyspace();
+	}
 
 	public Register_db() {
 
-		this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-		this.session = cluster.connect();
-	//	System.out.println("fff");
-
 	}
 
-	public void use_keyspace() {
+	public static void use_keyspace() {
 		String query = "use radiotaxi";
 		session.execute(query);
 	}
 
-	public void create_table() {
-		String query = "create table driver_info( first_name text, last_name text, username text PRIMARY KEY, password text, mobile_no text, license_no text, car_no text);";
+	public void createTable() {
+		String query = "CREATE TABLE driver_info( first_name text, last_name text, username text PRIMARY KEY, password text, mobile_no text, license_no text, car_no text);";
 		try {
 			session.execute(query);
 		} catch (AlreadyExistsException e) {
@@ -30,9 +36,9 @@ public class Register_db {
 		}
 	}
 
-	public void register_rider(String username, String first_name,
+	public void registerRider(String username, String first_name,
 			String last_name, String mobile_no, String password) {
-		String query = " insert into rider_info (username,first_name,last_name,mobile_no,password) values ('"
+		String query = " INSERT INTO rider_info (username,first_name,last_name,mobile_no,password) VALUES ('"
 				+ username
 				+ "','"
 				+ first_name
@@ -47,10 +53,10 @@ public class Register_db {
 		}
 	}
 
-	public void register_driver(String username, String first_name,
+	public void registerDriver(String username, String first_name,
 			String last_name, String mobile_no, String password, String car_no,
 			String license_no) {
-		String query = " insert into driver_info (username,first_name,last_name,mobile_no,password,car_no,license_no) values ('"
+		String query = " INSERT INTO driver_info (username,first_name,last_name,mobile_no,password,car_no,license_no) VALUES ('"
 				+ username
 				+ "','"
 				+ first_name
@@ -70,10 +76,54 @@ public class Register_db {
 			System.out.print(e);
 		}
 	}
+	
+	/*
+	  command to create bookings table -
+	    CREATE TABLE bookings (rider text,driver text,time timestamp,origin text,destination text,PRIMARY KEY(rider, time));
+	 */
+	public String registerBooking(String rider, String time, String origin, String destination) {
+		
+		// TODO: generate valid booking IDs
+		int randBookingId = 0 + (int) (Math.random()*100000);
+		
+		String query = "INSERT INTO bookings (booking_id,rider,driver,time,origin,destination,matched) VALUES ("
+				+ randBookingId
+				+ ",'"
+				+ rider
+				+ "','"
+				+ null // driver
+				+ "','"
+				+ time
+				+ "','"
+				+ origin
+				+ "','"
+				+ destination
+				+ "',"
+				+ false
+				+ ");";
+		try {
+			session.execute(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return query;
+	}
+	
+	public List<Row> getRides() {
+		String query = "SELECT * FROM bookings WHERE matched = false ALLOW FILTERING;";
+		List<Row> results = null;
+		try {
+			ResultSet rs = session.execute(query);
+			results = rs.all();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return results;
+	}
 
 	public boolean login(String username, String password, String type) {
 		if (type.equals("rider")) {
-			String query = "select * from rider_info where username='"
+			String query = "SELECT * FROM rider_info WHERE username='"
 					+ username + "' and password='" + password
 					+ "' allow filtering;";
 			try {
@@ -87,12 +137,12 @@ public class Register_db {
 			} catch (Exception e) {
 				System.out.print(e);
 			}
-			System.out.println("yolo");
+			System.out.println("returning false...");
 			return false;
 		} else {
-			String query = "select * from driver_info where username='"
-					+ username + "' and password='" + password
-					+ "' allow filtering;";
+			String query = "SELECT * FROM driver_info WHERE username='"
+					+ username + "' AND password='" + password
+					+ "' ALLOW FILTERING;";
 			try {
 				ResultSet rs = session.execute(query);
 				return (!rs.all().isEmpty());
@@ -104,7 +154,7 @@ public class Register_db {
 			} catch (Exception e) {
 				System.out.print(e);
 			}
-			System.out.println("yolo");
+			System.out.println("returning false...");
 			return false;
 		}
 	}
