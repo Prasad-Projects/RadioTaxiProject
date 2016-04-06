0574632de3e5e8063ff3560c7611bab4a825029a
Nisarg Shah <nisargshah323@gmail.com>
2016-04-07 00:17:07 +0530

    initialise database during registration

--- src/Register.java
+++ src/Register.java
@@ -1,11 +1,13 @@
 public class Register {
 
 	public void registerRider(Rider rider, String password) {
+		AccessDB.initialise();
 		AccessDB.use_keyspace();
 		AccessDB.registerRider(rider, password);
 	}
 
 	public void registerDriver(Driver driver, String password) {
+		AccessDB.initialise();
 		AccessDB.use_keyspace();
 		AccessDB.registerDriver(driver ,password);
 	}
