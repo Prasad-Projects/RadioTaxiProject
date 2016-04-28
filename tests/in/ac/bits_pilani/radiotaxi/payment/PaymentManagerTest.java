c3fbe3cac1af095996dad183cffa817cdf324aee
Nisarg Shah <nisargshah323@gmail.com>
2016-04-28 09:39:33 +0530

    add tests for PaymentManager

--- tests/in/ac/bits_pilani/radiotaxi/payment/PaymentManagerTest.java
+++ tests/in/ac/bits_pilani/radiotaxi/payment/PaymentManagerTest.java
@@ -1,8 +1,17 @@
 package in.ac.bits_pilani.radiotaxi.payment;
 
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
 import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.expectLastCall;
+
+import static org.easymock.EasyMock.anyInt;
+import static org.easymock.EasyMock.anyString;
+import static org.easymock.EasyMock.aryEq;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.easymock.EasyMockSupport;
 import org.junit.Ignore;
@@ -11,27 +20,49 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import in.ac.bits_pilani.radiotaxi.CabType;
 import in.ac.bits_pilani.radiotaxi.db.AccessDB;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(AccessDB.class)
 public class PaymentManagerTest extends EasyMockSupport {
 	
-	@Ignore
 	@Test
-	public void testMoneyTransfer() {
+	public void testMoneyTransfer() throws Exception {
+
+		CabType cab = CabType.Double;
+		String rider = "rider", distance = "1 kms", duration = "10 mins",
+				origin = "a", dest = "b";
+		float[] originCoord = new float[] {10.0f, 11.0f},
+				destCoord = new float[] {10.0f, 11.0f};
+		// get current time (format yyyy-mm-dd HH:mm:ss) 
+		LocalDateTime time = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
+        String timeStr = time.toString().replace('T', ' ').split("\\.")[0];		
+		
 		mockStatic(AccessDB.class);
+		/*expect*/ AccessDB.bookARide(anyString(), anyString(), anyString(), anyString(), anyInt(), aryEq(originCoord), aryEq(destCoord));
+		expectLastCall();
+		
+		replay(AccessDB.class);
+		
+		PaymentManager.moneyTransfer(rider, cab, distance, duration, origin, dest, timeStr, originCoord, destCoord);		
+		
+		verify(AccessDB.class);
+	}
+	
+	@Test
+	public void testBalanceCheck() {
+
+		mockStatic(AccessDB.class);
+		
 		expect(AccessDB.bookedCabBalance()).andReturn(1);
 		
 		replay(AccessDB.class);
 		
+		PaymentManager pm = new PaymentManager();
+		pm.balanceCheck();
 		
-	}
-	
-	@Ignore
-	@Test
-	public void testBalanceCheck() {
-		// TODO
+		verify(AccessDB.class);
 	}
 
 }
